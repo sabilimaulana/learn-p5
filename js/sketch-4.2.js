148bab7b2912dad58fc1067d8bb677bbf1815ffd
sabilimaulana <spacysbl.learn@gmail.com>
2022-02-14 19:51:23 +0700

    4.2: Nested Loops

---
+++ js/sketch-4.2.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  strokeWeight(4);
+  stroke(255);
+
+  for (let x = 0; x <= mouseX; x += 50) {
+    for (let y = 0; y <= height; y += 50) {
+      fill(random(255), 0, random(255));
+      ellipse(x, y, 25, 25);
+    }
+  }
+}
