ce71683264326766f5b8d64ae2bad03ea8f4e3b5
sabilimaulana <spacysbl.learn@gmail.com>
2022-02-16 14:30:03 +0700

    5.2: Function Parameters and Arguments

---
+++ js/sketch-5.2.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(50);
+
+  lollipop(100, 100, 50);
+  lollipop(300, 200, 150);
+}
+
+const lollipop = (x, y, diameter) => {
+  fill(0, 200, 255);
+  rect(x - 10, y, 20, 150);
+  fill(255, 0, 200);
+  ellipse(x, y, diameter, diameter);
+};
